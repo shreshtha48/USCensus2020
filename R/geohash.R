bbb24c23d861bf80de74b9e7c57f48a8c15db380
Shreshtha <modishreshtha48@gmail.com>
2022-09-13 17:44:50 +0530

    Add files via upload

---
+++ R/geohash.R
@@ -0,0 +1,20 @@
+#' @description This function provides the geohash for the given state and level
+#' @export
+#' @usage geohash(state,level)
+
+
+#Takes the state and the level that you want the data for in and returns the name and the geohash
+geohash=function(state,level=c("state","county","tract","block","blockgroup")){
+  #loading the required packages
+  require(paste("UScensus",level,"20",sep=""))
+  require(sf)
+  require(geohash)
+  #loading the data
+  x=paste(state,level,"20",sep="")
+  data=data(list=x,envir=.GlobalEnv,package=paste("UScensus",level,"20",sep=""))
+  #getting the centroids and then converting them to geohash using the coordinates
+  centroid=st_centroid(data[, (colnames(data) %in% c('geometry','NAME'))])
+  centroid=as.data.frame(st_coordinates(centroid))
+  geohash=gh_encode(lats=centroid$X,lngs = centroid$Y)
+  return(geohash)
+}

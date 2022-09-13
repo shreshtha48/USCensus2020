47f6fc75182a328c01f2e72ff193b4c42063e031
Shreshtha <modishreshtha48@gmail.com>
2022-09-14 00:49:57 +0530

    Update geohash.R

--- R/geohash.R
+++ R/geohash.R
@@ -11,10 +11,13 @@ geohash=function(state,level=c("state","county","tract","block","blockgroup")){
   require(geohash)
   #loading the data
   x=paste(state,level,"20",sep="")
-  data=data(list=x,envir=.GlobalEnv,package=paste("UScensus",level,"20",sep=""))
+  data(list=x,envir=.GlobalEnv,package=paste("UScensus",level,"20",sep=""))
+  temp <- get(x)
+  centroid=temp[, c("NAME","geometry")]
   #getting the centroids and then converting them to geohash using the coordinates
-  centroid=st_centroid(data[, (colnames(data) %in% c('geometry','NAME'))])
+  centroid=st_centroid(centroid)
   centroid=as.data.frame(st_coordinates(centroid))
   geohash=gh_encode(lats=centroid$X,lngs = centroid$Y)
   return(geohash)
 }
+geohash(state="alabama",level="county")

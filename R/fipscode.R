7c97f15b997fa4a19a7bed96b8f671f61ef202e1
Shreshtha <modishreshtha48@gmail.com>
2022-09-15 08:12:30 +0530

    Added the county fips

--- R/fipscode.R
+++ R/fipscode.R
@@ -1,21 +1,31 @@
-fipsdf <- data.frame(State = c("Alabama", "Alaska", "Arizona", "Arkansas", 
-                               "California", "Colorado", "Connecticut", "Delaware", "Florida", 
-                               "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", 
-                               "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", 
-                               "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", 
-                               "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", 
-                               "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", 
-                               "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", 
-                               "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", 
-                               "West Virginia", "Wisconsin", "Wyoming"), FIPS = c("01", "02", 
-                                                                                  "04", "05", "06", "08", "09", "10", "12", "13", "15", "16", "17", 
-                                                                                  "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", 
-                                                                                  "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", 
-                                                                                  "40", "41", "42", "44", "45", "46", "47", "48", "49", "50", "51", 
-                                                                                  "53", "54", "55", "56"), stringsAsFactors = FALSE)
-get_state_fips <- function(state, lookupvector = getfips){
-  getfips <- fipsdf$FIPS
-  fipscode <- unname(lookupvector[state])
-  return(fipscode)
+#' @description This variable takes the name of the state and returns the fips code at the county level
+#' @param state the name of the state
+#' @param countyname the name of the county, please provide the exact county name e.g. for baldwin county in alabama, "Baldwin County"
+#' @param level the level that you want the data for i.e. state if want the fips code for the state or county if want for county
+#' @export
+get_fips <- function(state,countyname=NULL,level=c("state","county")){
+  #checks if the state is proper
+  if(length(state)>0&check_state(state)=state){
+    #loads the data statefips, consists of the state name and the fips code. Since the county df only has fips code, need them, for level=state, this returns the fips code
+    data("statefips",envir=.GlobalEnv)
+    #subsetting based on value and returns the fips code
+    fips<-fipsdf[(fipsdf$State%in%state==TRUE),]
+    fips= fips[,2]
+    if(level=="county"){
+      #loading the county df
+      data("countyfips",envir=.GlobalEnv)
+      #making sure that the county name is in the right format
+      countyname=gsub("(^|[[:space:]])([[:alpha:]])", "\\1\\U\\2",
+                      countyname,
+                      perl = TRUE)
+      #this function doesnt work for some reason, need to fix this
+      countycode<-data_filter(countyfips,var=c("Statefips","Name"),values=c(fips,countyname))
+      #the returned variable will have the fips code of the county, name of the county and fips of the state the county is in
+      return(countycode)
+    }
+    else{
+      return(fips)
+    }
+  }
 }
-get_state_fips(state=Alabama)
+
